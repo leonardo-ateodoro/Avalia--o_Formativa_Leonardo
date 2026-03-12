50ccf1525d04d002cd7881f138520cf8aa03529d
leonardo <leonardo.a.teodoro@aluno.senai.br>
2026-03-12 16:55:17 -0300

    funçao delete

--- models/requisito.py
+++ models/requisito.py
@@ -7,3 +7,11 @@ def recuperar_requisitos():
     requisitos = cursor.fetchall()
     conexao.close()
     return requisitos
+
+def excluir_requisitos(codigo):
+    conexao, cursor = conectar()
+    cursor.execute("""
+                    DELETE FROM tb_requisitos WHERE cod_requisitos = %s;
+                    """, [codigo])
+    conexao.commit()
+    conexao.close()
